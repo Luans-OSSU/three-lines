c368fffd2c0b32fe2395e2f1e60c07ebd9331bfe
danite <luansnain@gmail.com>
2021-04-04 17:02:08 +0200

    Refactor Player struct into ECS and add system for physics and animation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod animator;
+mod components;
+mod physics;
+
 use sdl2::event::Event;
 use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
@@ -5,28 +9,15 @@ use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
 
+use specs::{Builder, World, WorldExt};
+
 use std::time::Duration;
+
+use crate::components::*;
 
 const PLAYER_MOVEMENT_SPEED: i32 = 5;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-#[derive(Debug)]
-struct Player {
-    position: Point,
-    sprite: Rect,
-    speed: i32,
-    direction: Direction,
-    current_frame: i32,
-}
-
-fn get_direction_spritesheet_row(direction: Direction) -> i32 {
+fn get_spritesheet_row_from_direction(direction: Direction) -> i32 {
     use self::Direction::*;
     match direction {
         Up => 3,
@@ -35,6 +26,33 @@ fn get_direction_spritesheet_row(direction: Direction) -> i32 {
         Right => 2,
     }
 }
+
+fn create_character_animation_frames(
+    spritesheet: usize,
+    initial_frame: Rect,
+    direction: Direction,
+) -> Vec<Sprite> {
+    let (frame_width, frame_height) = initial_frame.size();
+    let y_offset =
+        initial_frame.y() + frame_height as i32 * get_spritesheet_row_from_direction(direction);
+
+    let mut frames = Vec::new();
+
+    for index in 0..3 {
+        frames.push(Sprite {
+            spritesheet,
+            region: Rect::new(
+                initial_frame.x() + frame_width as i32 * index,
+                y_offset,
+                frame_width,
+                frame_height,
+            ),
+        })
+    }
+
+    frames
+}
+
 fn render(
     canvas: &mut WindowCanvas,
     color: Color,
@@ -49,7 +67,8 @@ fn render(
     let (frame_width, frame_height) = player.sprite.size();
     let current_frame = Rect::new(
         player.sprite.x() + frame_width as i32 * player.current_frame,
-        player.sprite.y() + frame_height as i32 * get_direction_spritesheet_row(player.direction),
+        player.sprite.y()
+            + frame_height as i32 * get_spritesheet_row_from_direction(player.direction),
         frame_width,
         frame_height,
     );
@@ -63,19 +82,6 @@ fn render(
     Ok(())
 }
 
-fn update_player(player: &mut Player) {
-    use self::Direction::*;
-    match player.direction {
-        Left => player.position = player.position.offset(-player.speed, 0),
-        Right => player.position = player.position.offset(player.speed, 0),
-        Up => player.position = player.position.offset(0, -player.speed),
-        Down => player.position = player.position.offset(0, player.speed),
-    }
-
-    if player.speed != 0 {
-        player.current_frame = (player.current_frame + 1) % 3;
-    }
-}
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -94,15 +100,46 @@ fn main() -> Result<(), String> {
         .expect("could not make a canvas");
 
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("assets/bardo.png")?;
+    let textures = [texture_creator.load_texture("assets/bardo.png")?];
 
-    let mut player = Player {
-        position: Point::new(0, 0),
-        sprite: Rect::new(0, 0, 26, 36),
-        speed: 0,
-        direction: Direction::Right,
+    let player_spritesheet = 0;
+    let player_initial_frame = Rect::new(0, 0, 26, 36);
+    let player_animation = MovementAnimation {
         current_frame: 0,
+        up_frames: create_character_animation_frames(
+            player_spritesheet,
+            player_initial_frame,
+            Direction::Up,
+        ),
+        down_frames: create_character_animation_frames(
+            player_spritesheet,
+            player_initial_frame,
+            Direction::Down,
+        ),
+        left_frames: create_character_animation_frames(
+            player_spritesheet,
+            player_initial_frame,
+            Direction::Left,
+        ),
+        right_frames: create_character_animation_frames(
+            player_spritesheet,
+            player_initial_frame,
+            Direction::Right,
+        ),
     };
+
+    let mut world = World::new();
+
+    world
+        .create_entity()
+        .with(Position(Point::new(0, 0)))
+        .with(Velocity {
+            speed: 0,
+            direction: Direction::Right,
+        })
+        .with(player_animation.right_frames[0].clone())
+        .with(player_animation)
+        .build();
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -174,7 +211,6 @@ fn main() -> Result<(), String> {
 
         // Later we'll be used for update()
         i = (i + 1) % 255;
-        update_player(&mut player);
 
         render(&mut canvas, Color::RGB(i, 125, 255 - i), &texture, &player)?;
 
